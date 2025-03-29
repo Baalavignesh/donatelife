3548bd9744539b56d79d93f9da9f5858ec444f52
samuelsowpati <sowpatisamuel@gmail.com>
2025-03-29 18:21:38 -0400

    Updated dashboard and card components with STATIC rendering and improved layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Error from "./pages/error";
-import PostReq from "./pages/postreq"; 
+import PostReq from "./pages/postreq";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dashboard" Component={Dashboard} />
-        <Route path="/register" Component={Register} />
-        <Route path="/postreq" Component={PostReq} /> {/* ✅ New route */}
-        <Route path="/" Component={Login} />
-        <Route path="*" Component={Error} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/postreq" element={<PostReq />} />
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
